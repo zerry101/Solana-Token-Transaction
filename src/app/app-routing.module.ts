c32644bea84ffd63ddb35f63a840838345c20750
zerry101 <macone394@gmail.com>
2023-03-24 20:20:33 +0530

    code modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import * as web3 from '@solana/web3.js';
+import { Connection, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js';
+// import { TokenListProvider } from '@solana/spl-token-registry';
+// import { Token } from '@solana/spl-token';
 
+
+// import { Connection, PublicKey, Transaction } from '@solana/web3.js';
+// import { TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
 const routes: Routes = [];
 
 @NgModule({
